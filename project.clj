d7f04b3fef5adad76da35dcf18cd70c59e8f74ec
Ivan Grishaev <ivan@grishaev.me>
2023-02-03 16:42:45 +0300

    initial

---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject dynamodb "0.1.0-SNAPSHOT"
+
+  :description
+  "FIXME: write description"
+
+  :url
+  "http://example.com/FIXME"
+
+  :license
+  {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+   :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :dependencies
+  [[http-kit "2.6.0"]
+   [cheshire "5.10.0"]
+   [clj-aws-sign "0.1.1"]]
+
+  :profiles
+  {:dev
+   {:dependencies
+    [[org.clojure/clojure "1.11.1"]]}
+
+   :uberjar
+   {:aot :all
+    :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
