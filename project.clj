bbbf5f733035ea324097aa0b91c61930acb3f4c6
Ivan Grishaev <ivan@grishaev.me>
2023-02-11 15:02:40 +0300

    updated

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dynamodb "0.1.0-SNAPSHOT"
+(defproject com.github.igrishaev/dynamodb "0.1.0-SNAPSHOT"
 
   :description
   "DynamoDB in pure Clojure. GraalVM-friendly"
@@ -6,9 +6,23 @@
   :url
   "https://github.com/igrishaev/dynamodb"
 
+  :deploy-repositories
+  {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
+
   :license
   {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
    :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :release-tasks
+  [["vcs" "assert-committed"]
+   ["test"]
+   ["change" "version" "leiningen.release/bump-version" "release"]
+   ["vcs" "commit"]
+   ["vcs" "tag" "--no-sign"]
+   ["deploy"]
+   ["change" "version" "leiningen.release/bump-version"]
+   ["vcs" "commit"]
+   ["vcs" "push"]]
 
   :plugins
   [[lein-cljfmt "0.9.2"]]
